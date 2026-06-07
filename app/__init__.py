802fbcc597bfcc67a9c39aff4f3468a3f3528957
juanza <juan.zapata22871@ucaldas.edu.co>
2026-06-07 02:34:10 -0500

    chore(main): include routers to sensor, zone and monitoring

---
+++ app/__init__.py
@@ -0,0 +1,1 @@
+from app.routers import sensors, zones, monitorings
--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.routers import sensors, zones, monitorings
 
-app = FastAPI()
+app = FastAPI(title="Sensores y Zonas de Monitoreo Industrial")
+
+app.include_router(sensors.router)
+app.include_router(zones.router)
+app.include_router(monitorings.router)
 
 @app.get("/")
 def root():
