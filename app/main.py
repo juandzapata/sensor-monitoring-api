0a0b14c6277a551b6122f34776bf64109892f43b
juanza <juan.zapata22871@ucaldas.edu.co>
2026-06-07 14:03:01 -0500

    feat(main): add CROS Middleware for frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import sensors, zones, monitorings
 
 app = FastAPI(title="Sensores y Zonas de Monitoreo Industrial")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(sensors.router)
 app.include_router(zones.router)
